388da961251d23bcf4fd054579afe64131248dea
aumartines <acm_asd@hotmail.com>
2022-05-15 18:01:17 -0600

    Vue Startup

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+$_SESSION["premium"] = true;
+ 
+?>
+<!doctype html>
+<html>
+  <head>
+    <title>Una pagina</title>
+    <script src="js/script.js"></script>
+  </head>
+  <html>
+    <h1>Un titulo</h1>
+    
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+    </p>
+  </html>
+</html>
